ad83615624635abaff81044e42e5f16fd5e9d472
Aditya Sethi <100475196+adityasethi-in@users.noreply.github.com>
2022-05-22 16:27:35 +0530

    Return function type 2

---
+++ return function type 2.py
@@ -0,0 +1,14 @@
+
+a=int(input("enter number 1"))
+
+b=int(input("enter number 2"))
+
+c=int(input("enter number 3"))
+
+
+def maths(a,b,c):
+    x=a+b+c
+    y=a*b*c
+    return x,y
+
+maths(a,b,c)
